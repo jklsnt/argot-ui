0dfa4135dcd8d0f4af730fb8b087de419ea7e862
Houjun Liu <houjun@jemoka.com>
2023-12-18 23:52:32 -0800

    nya UwU

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let obj;
+</script>
+
+<div style="border: 1px dotted gray; margin: 10px 0; padding: 15px;">
+    <a href={obj.link} style="text-decoration: none; cursor:pointer"><span>{obj.title}</span> <span style="opacity: 0.4; color: blue; font-size: 10px"> {(new URL(obj.link)).hostname}</span></a>
+    <div style="float:right; font-size: 1; display: flex; flex-direction: column; gap: 3px">
+        <span style="text-align: right;">{obj.author}</span>
+        <span style="font-size: 13px;">{obj.time}</span>
+    </div>
+    <div style="margin-top:5px">
+    {#if obj.content != null}
+    <div>{obj.content}</div>
+    {:else}
+        <div style="opacity:0.3">Usuwu did nyot suwubmit contenty-wontent~!</div>
+    {/if}
+    </div>
+
+</div>    
+
+
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+    let messages = [
+        "Nyaa~! Hewe's a wittle link to a pwecious linkie-poo winkie! OwO giggles",
+        "Uwaa~! This winkie-linkie is making my heart go doki-doki! (*≧ω≦)",
+        "Look at this adorable wittle linkie-winkie! (* ^ ω ^)",
+    ];
+
+    let message = messages[Math.floor(Math.random() * messages.length)];
+
+    let links = [
+        {
+            "title": "ChickenGPT",
+            "link": "https://chickengpt.com",
+            "author": "jemoka",
+            "time": "two minutes ago",
+            "content": null
+        },
+        {
+            "title": "Assembly micro-optimizations",
+            "link": "https://example2.com",
+            "author": "quantumish",
+            "time": "five minutes ago",
+            "content": "I'm a big potato."
+        },
+        {
+            "title": "Assembly micro-optimizations",
+            "link": "https://example2.com",
+            "author": "quantumish",
+            "time": "four hours ago",
+            "content": "UwU~! (ฅ^・ω・^ ฅ) Nyaa~! In the whimsical realm of JavaScript, imagine a playful dance with the elusive DateTimeFormat object. With a sprinkle of coding magic, the current date and time gracefully transform into a delightful string, adorned in the format of hh:mm:ss mm/dd/yyyy. This enchanting snippet creates a mesmerizing output, offering a glimpse into a world where digits waltz harmoniously, revealing a charming display like 08:30:45 01/25/2023. The script invites you to tailor the date object to your liking, ensuring a personalized touch to this joyous ballet of time formatting. Nyaa~ (∩^ω^∩)"
+        },
+        {
+            "title": "Fast code tricks",
+            "link": "https://example.com",
+            "author": "quantumish",
+            "time": "seven days ago",
+            "content": null
+        }
+    ]
+
+    import Link from "$lib/components/link.svelte";
+    
+</script>    
+
+<div style="">
+    <h1>Argot</h1>
+    <i>{message}</i>
+
+    <hr />
+
+    <div style="display: flex; justify-content: space-between; align-items:center"><h2>winkie-linkies OwO</h2> <button style="max-height: 25px">UwU~! Suwubmit winky-wink!</button></div>
+
+    {#each links as link}
+        <Link obj={link} />
+    {/each}
+
+</div>
