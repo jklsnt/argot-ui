bcc74f0a4fc1c7bbf19c05a528eb29519816ba23
Houjun Liu <houjun@jemoka.com>
2023-12-21 00:22:46 -0800

    mmm

--- src/routes/posts/[post]/+page.svelte
+++ src/routes/posts/[post]/+page.svelte
@@ -11,79 +11,8 @@
     let reply = "";
 
     async function fetchPost(postID) {
-        console.log(`fetching ${postID}`)
-        // const response = await self.fetch(`${server}/posts`)
-        // return response.json();	
-        return {
-            "author": "quantumish",
-            "comments": [
-                {
-                    "author": "quantumish",
-                    "children": [
-                        {
-                            "author": "quantumish",
-                            "children": [{
-                            "author": "quantumish",
-                            "children": [],
-                            "content": "Damn that's crazy. Engage in the most adowable and cozy dwiscussion, where thoughts awe expwessed with the utmost kawaii vibes! (* >ω<) Conversations unfurl like gentle nyuzzles, as pwecious ideas dance together in the soft moonlight of uwu. Each topic is embraced with a warm hug of nya-magic, turning mundane discussions into an enchanting journey through the whimsical land of kawaii communication. So, let's gathew awound, nya-friends, and embark on a delightful adventure of dwiscovewy and nyuance together! (´∩｡• ᵕ •｡∩`) Nyaa~ (∩^ω^∩)",
-                            "id": 2,
-                            "private": false,
-                            "time": "7 hours ago"
-                            }, {
-                            "author": "quantumish",
-                            "children": [],
-                            "content": "this is an ice cone",
-                            "id": 2,
-                            "private": false,
-                            "time": "7 hours ago"
-                        }],
-                            "content": "Damn that's crazy. Engage in the most adowable and cozy dwiscussion, where thoughts awe expwessed with the utmost kawaii vibes! (* >ω<) Conversations unfurl like gentle nyuzzles, as pwecious ideas dance together in the soft moonlight of uwu. Each topic is embraced with a warm hug of nya-magic, turning mundane discussions into an enchanting journey through the whimsical land of kawaii communication. So, let's gathew awound, nya-friends, and embark on a delightful adventure of dwiscovewy and nyuance together! (´∩｡• ᵕ •｡∩`) Nyaa~ (∩^ω^∩)",
-                            "id": 2,
-                            "private": false,
-                            "time": "7 hours ago"
-                        },
-                        {
-                            "author": "quantumish",
-                            "children": [],
-                            "content": "Damn that's crazy. Engage in the most adowable and cozy dwiscussion, where thoughts awe expwessed with the utmost kawaii vibes! (* >ω<) Conversations unfurl like gentle nyuzzles, as pwecious ideas dance together in the soft moonlight of uwu. Each topic is embraced with a warm hug of nya-magic, turning mundane discussions into an enchanting journey through the whimsical land of kawaii communication. So, let's gathew awound, nya-friends, and embark on a delightful adventure of dwiscovewy and nyuance together! (´∩｡• ᵕ •｡∩`) Nyaa~ (∩^ω^∩)",
-                            "id": 2,
-                            "private": false,
-                            "time": "7 hours ago"
-                        }
-                    ],
-                    "content": "Deep personal info.",
-                    "id": 1,
-                    "private": true,
-                    "time": "7 hours ago"
-                },
-                {
-                    "author": "quantumish",
-                    "children": [{
-                    "author": "quantumish",
-                    "children": [],
-                    "content": "this is NOT an ICE CREAM CONe.",
-                    "id": 3,
-                    "private": false,
-                    "time": "7 hours ago"
-                }],
-                    "content": "Boring stuff.",
-                    "id": 3,
-                    "private": false,
-                    "time": "7 hours ago"
-                }
-            ],
-            "content": null,
-            "id": 1,
-            "link": "https://example.com",
-            "private": false,
-            "tags": [
-                "asm",
-                "cool",
-                "wack"
-            ],
-            "time": "7 hours ago",
-            "title": "Whee"
-        }
+        const response = await self.fetch(`${server}/posts/${postID}`)
+        return response.json();	
     }
 
     async function submit(id) {
