a9664d092e5f2c72b5a60c54069b70c288ebba16
quantumish <freifeld.david@gmail.com>
2023-12-21 20:38:05 -0800

    Tag addition in submit page

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -10,8 +10,17 @@
     let content = "";
     let priv = false;
 
+    let curtag = "";
+    let tags = [];
+  
     import Link from "$lib/components/link.svelte";
 
+    async function fetchTags() {	   
+	    const response = await self.fetch(`${server}/tags`, {credentials: 'include'})
+	    return response.json();
+	}
+    let promise = fetchTags();
+  
     async function submit() {
         await self.fetch(`${server}/posts`,
                          {method: "POST",
@@ -23,9 +32,10 @@
                               title: title,
                               link: url,
                               content: content,
+							  private: priv,
+							  tags: tags.map((t) => t.id)
                           })});
-        window.location.href = "/";
-        
+        window.location.href = "/";      
     }
 </script>
 
@@ -38,9 +48,22 @@
     <ul>
         <li>URL (* ω): <input type="text" bind:value={url} /></li>
         <li>Titwe (*≧▽≦):: <input type="text" bind:value={title} /></li>
-        <li>Content (⁄ ⁄•⁄ω⁄•⁄ ⁄): <textarea bind:value={content}></textarea></li>
-		<li>Private?: <input type="checkbox" bind:checked={priv}/></li>
+        <li>Content (⁄ ⁄•⁄ω⁄•⁄ ⁄): <br><textarea style="width: 400px;"bind:value={content}></textarea></li>
+		<li>Private?: <input type="checkbox" bind:checked={priv}/></li>		
     </ul>
+	Tags:
+	{#await promise}
+      <p>Loading... :3</p>
+    {:then avail_tags}
+	  <select bind:value={curtag}>
+		{#each avail_tags as tag}
+		  <option value="{tag}">{tag.name}</option>
+		{/each}
+	  </select>
+	{/await}
+	<button on:click={() => {tags.push(curtag); tags=tags; }}>+</button><br>	
+
+	
     <hr />
 
     <!-- <div style="display: flex; justify-content: space-between; align-items:center"><h2>winkie-linkies OwO</h2> <button style="max-height: 25px; cursor: pointer" on:click={() => {window.location.href="/submit"}}>UwU~! Suwubmit winky-wink!</button></div> -->
@@ -50,7 +73,7 @@
     <!-- {/each} -->
 
     Pwease dewight in this exclusive linkie cuwated by none othew than meowself:
-    <Link obj={{title: "​"+title, link:url, author:"you", content: content, time:"just now", tags: [], private: priv}} />
+    <Link obj={{title: "​"+title, link:url, author:"you", content: content, time:"just now", tags: tags.map((t) => t.name), private: priv}} />
     <hr />
     <button on:click={submit}>UwU~! (*≧ω≦) Suwubmit this wink nyow, desu~! (´•̥ ω •̥`) Nyaa~ (∩^ω^∩)</button>
 </div>
