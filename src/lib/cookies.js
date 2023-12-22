0f6b6185248d8cb83583c2e6ed325ff080597474
Houjun Liu <houjun@jemoka.com>
2023-12-21 21:15:08 -0800

    decode

--- src/lib/cookies.js
+++ src/lib/cookies.js
@@ -6,8 +6,8 @@ export function setCookie(c_name, value, exdays) {
 }
 
 export function getCookie(name) {
-      const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
 }
 
