8be5abc33f8a92c48b12d4b55611826aad819b91
quantumish <freifeld.david@gmail.com>
2023-12-21 21:36:30 -0800

    Start of about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<h1> about </h1>
+
+Argot is a private link aggregator (think Hacker News or Lobste.rs clone).
+
+<ul>
+  <li><b>Tagging support.</b> Full tagging system with decently powerful queries.</li>
+  <li><b>No votes.</b></li>
+  <li></li>
+</ul>
