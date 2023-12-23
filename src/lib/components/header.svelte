a1bbc18c8cf2a2478cf1221a06ebb0c6cf2b8367
quantumish <freifeld.david@gmail.com>
2023-12-23 15:25:55 -0800

    Show inbox

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,13 +3,19 @@
     import {messages, server} from "$lib/constants.js";
     import { getCookie, deleteCookie } from "$lib/cookies.js";
     let message = messages[Math.floor(Math.random() * messages.length)];
-
+ 
     let user = {};
     onMount(async () => {
         let cookie = getCookie("argot__usr");
         user = (cookie && cookie != "") ? JSON.parse(cookie) : {};
     });
-    
+
+  
+    async function fetchInboxSz() {	   
+        const response = await self.fetch(`${server}/inbox/size`, {credentials: 'include'})
+	    return response.json();	  
+    }
+  
     $: isLogged = user.nick != undefined;
 
     async function logout() {	   
@@ -19,12 +25,15 @@
     }
 
     import "./header.css";
-    
+
+    $: promise = fetchInboxSz();
 </script>    
 
 <div>
     <div class="header-callout">
         <a href="/"><div style="display: inline-block;" class="square">&nbsp;</div><h1 style="display: inline-block">Argot</h1></a>
     </div>
-    <div><a class="header-user" href={!isLogged ?"javascript:void(0)":`users/${user.nick}`}>{isLogged?`${user.nick}`:"unauthenticated"}</a> <span class="menu" style="float:right; "><a href="/search">search</a> | <a href="/tags">tags</a> | <a href="/login" on:click={() => {if(isLogged) logout();}}>{isLogged ? "logout" : "login"}</a></span> </div>
+    <div><a class="header-user" href={!isLogged ?"javascript:void(0)":`/inbox`}>{isLogged?`${user.nick}`:"unauthenticated"}
+  {#await promise}{:then inbox}{#if inbox.size != 0}({inbox.size}){/if}{/await}</a>
+	  <span class="menu" style="float:right; "><a href="/search">search</a> | <a href="/tags">tags</a> | <a href="/login" on:click={() => {if(isLogged) logout();}}>{isLogged ? "logout" : "login"}</a></span> </div>
 </div>
